e32560add46c7c50490f062fa5dd82d73b23ba23
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2021-06-13 12:51:40 +0100

    a

--- testingStocks.cs
+++ testingStocks.cs
@@ -1,8 +1,17 @@
 using System;
 using System.Net;
 
-public string Symbol = "LLOY.L";
-//public string Market = "LSE";
 
-webRequest webRequest1 = new webRequest($"finance.yahoo.com/quote/{Symbol}");
-Console.Write(webRequest1.getResponse());
+namespace ImgurRandom
+{
+    class Program
+    {
+      public string Symbol = "LLOY.L";
+      //public string Market = "LSE";
+
+      static Task Main(){
+      WebRequest webRequest1 = WebRequest.Create($"https://finance.yahoo.com/quote/{Symbol}");
+      WebResponse response = webRequest1.GetResponse();
+      Console.Write(response);}
+    }
+ }
