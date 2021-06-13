1cfc18439049d8a235e5ab7cecba2cf425c72e25
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2021-06-13 12:40:18 +0100

    test

--- testingStocks.cs
+++ testingStocks.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Net;
 
-public string Symbol = "LLOY";
-public string Market = "LON";
+public string Symbol = "LLOY.L";
+//public string Market = "LSE";
+
+webRequest webRequest1 = new webRequest($"finance.yahoo.com/quote/{Symbol}")
