47e00f2c1f8f4e6bc4c073bb43088c5d3dc02f9f
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2021-06-13 20:01:41 +0100

    Formatted Output to be easier to read

--- ConsoleApp1/debugging.cs
+++ ConsoleApp1/debugging.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Net.Http;
-using HtmlAgilityPack;
-using System.Threading.Tasks;
+﻿using HtmlAgilityPack;
+using System;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace StockInfo
 {
@@ -11,7 +11,8 @@ namespace StockInfo
         static readonly HttpClient client = new HttpClient();
         static async Task Main(string[] args)
         {
-           while(true){
+            while (true)
+            {
                 Console.Write("Enter Symbol: ");
                 await getInfo(Console.ReadLine());
             }
@@ -22,16 +23,18 @@ namespace StockInfo
             HttpResponseMessage r = await client.GetAsync($"https://finance.yahoo.com/quote/{Symbol}");
             HtmlDocument htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(await r.Content.ReadAsStringAsync());
-            var Currency = htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[2]/div[1]/div[2]/span").InnerText;
+            var CurrencyRef = htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[2]/div[1]/div[2]/span").InnerText;
+            var Currency = new String(CurrencyRef.Skip(CurrencyRef.Length - 3).Take(3).ToArray());
 
+            Console.WriteLine();
             Console.Write(htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[2]/div[1]/div[1]/h1").InnerText); // NAME
-            Console.WriteLine(" "+Currency.Split("-")[0]); // MARKET
-
-            Console.Write(htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"quote-header-info\"]/div[3]/div[1]/div/span[1]").InnerText); // PRICE
-            Console.Write(" "+new string(Currency.Skip(Currency.Length - 3).Take(3).ToArray())); // CURRENCY
-            Console.WriteLine("  "+htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div/span[2]").InnerText); // PERCENTAGE CHANGE
-
-            Console.WriteLine(htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[5]/td[2]").InnerText); // TODAYS RANGE
+            Console.WriteLine(" " + CurrencyRef.Split("-")[0]); // MARKET
+            Console.WriteLine();
+            Console.Write("Price: "+htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"quote-header-info\"]/div[3]/div[1]/div/span[1]").InnerText + " "+ Currency); // PRICE
+            Console.WriteLine("  " + htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div/span[2]").InnerText); // PERCENTAGE CHANGE
+            Console.WriteLine("Range: "+htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[5]/td[2]").InnerText+ " " + Currency); // TODAYS RANGE
+            Console.WriteLine("Open: " + htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[2]/td[2]/span").InnerText + " " + Currency); // OPEN
+            Console.WriteLine("Previous Close: " + htmlDocument.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[1]/td[2]/span").InnerText + " " + Currency);
 
             Console.WriteLine();
         }
