cb7f2cd8a857d3133f2bbd9a2ba10c0976a21fd9
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2021-06-13 13:02:54 +0100

    ;

--- testingStocks.cs
+++ testingStocks.cs
@@ -6,12 +6,14 @@ namespace ImgurRandom
 {
     class Program
     {
-      public string Symbol = "LLOY.L";
-      //public string Market = "LSE";
+        public string Symbol = "LLOY.L";
+        //public string Market = "LSE";
 
-      static Task Main(){
-        WebRequest webRequest1 = WebRequest.Create($"https://finance.yahoo.com/quote/{Symbol}");
-        WebResponse response = webRequest1.GetResponse();
-        Console.Write(response);}
+        static Task Main()
+        {
+            WebRequest webRequest1 = WebRequest.Create($"https://finance.yahoo.com/quote/{Symbol}");
+            WebResponse response = webRequest1.GetResponse();
+            Console.Write(response);
+        }
     }
- }
+}
